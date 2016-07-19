d6af148819cee84f05648dbc79db34830a09d762
jiejing.yang <jiejing.yang@B106-15.soit.local>
2016-07-20 09:24:50 +1200

    added another object

--- Assignment1/src/MyMain.java
+++ Assignment1/src/MyMain.java
@@ -3,8 +3,11 @@ public class MyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PersonInfo person1 = new PersonInfo ("John Doe","0123456","America");
-		person1.displayInfo();//click on "dot" pop up optional list->one line
+		PersonInfo person1 = new PersonInfo ("John Doe","0123456","America1");
+		person1.displayInfo();//click on "dot" pop up optional list->one line->Run
+		
+		PersonInfo person2 = new PersonInfo ("Jane Clark","45678","Canada2");
+		person2.displayInfo();
 	}
 
 }
