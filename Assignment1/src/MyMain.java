30f6a0f3531c10228049a898257589522c01b622
jiejing.yang <jiejing.yang@B106-15.soit.local>
2016-07-20 09:16:39 +1200

    created Two classes

---
+++ Assignment1/src/MyMain.java
@@ -0,0 +1,10 @@
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PersonInfo person1 = new PersonInfo ("John Doe","0123456","America");
+		person1.displayInfo();//click on "dot" pop up optional list->one line
+	}
+
+}
